5dee675462fb33afe8d5686c349fbc206c4bdf44
fmutant <funnymutant@gmail.com>
2015-01-30 11:39:28 +0300

    prototype ready

--- Problem Sets/Problem Set 3/student_func.cu
+++ Problem Sets/Problem Set 3/student_func.cu
@@ -3,9 +3,9 @@
 
 __global__ void calc(const float* const d_in, int *d_bins, unsigned int* const d_cdf, float* d_min, float* d_max, size_t numRows, size_t numCols, size_t numBins)
 {
-   /*
    size_t threadPos = threadIdx.x + blockDim.x * blockIdx.x;
    float curIn = d_in[threadPos];
+   
    //step 1
    while(*d_min > curIn)
    {
@@ -17,24 +17,23 @@ __global__ void calc(const float* const d_in, int *d_bins, unsigned int* const d
       *d_max = curIn;
       __threadfence();
    }
+   __syncthreads();
    float curMin = *d_min, curMax = *d_max;
    
-   printf("step 2");
    //step 2
-   float logLumRange = curMax - curMin;
-
+   int result = ((curIn - curMin) / (curMax - curMin)) * (float)numBins;
+   
    //step 3
-   printf("step 3");
-   size_t bin = min((unsigned int)(numBins - 1), (unsigned int)((curIn - curMin) / logLumRange * numBins));
+   int bin = min((int)numBins - 1, result);
    atomicAdd(&d_bins[bin], 1);
    __syncthreads();
-   
+  
    //step 4
-   printf("step 4");
-   if (0 == threadPos || threadPos >= numBins)
+   if (0 == threadPos || threadPos > numBins - 1)
       return;
-   d_cdf[threadPos] = d_cdf[threadPos - 1] + d_bins[threadPos - 1];
-   */
+
+   //scan should go here
+   //d_cdf[threadPos] = d_bins[threadPos - 1] + d_cdf[threadPos - 1];
 }
 
 void your_histogram_and_prefixsum(const float* const d_logLuminance,
@@ -50,18 +49,17 @@ void your_histogram_and_prefixsum(const float* const d_logLuminance,
    checkCudaErrors(cudaMalloc(&d_bins,   sizeof(int) * numBins));
    checkCudaErrors(cudaMemset(d_bins, 0,   sizeof(int) * numBins));
    checkCudaErrors(cudaMalloc(&d_min,   sizeof(float)));
-   checkCudaErrors(cudaMemset(d_min, 0,   sizeof(float)));
+   checkCudaErrors(cudaMemcpy(d_min, &min_logLum, sizeof(float), cudaMemcpyHostToDevice));
    checkCudaErrors(cudaMalloc(&d_max,   sizeof(float)));
-   checkCudaErrors(cudaMemset(d_max, 0,   sizeof(float)));
+   checkCudaErrors(cudaMemcpy(d_max, &max_logLum, sizeof(float), cudaMemcpyHostToDevice));
    
    const size_t length = numRows * numCols;
-   printf("%i\t%i\t%i\n", numBins, length, length / numBins);
-   calc<<<numBins, length / numBins>>>(d_logLuminance, d_bins, d_cdf, &min_logLum, &max_logLum, numRows, numCols, numBins);
+   calc<<<numBins, length / numBins>>>(d_logLuminance, d_bins, d_cdf, d_min, d_max, numRows, numCols, numBins);
    checkCudaErrors(cudaGetLastError());
-   //min_logLum = *d_min;
-   //max_logLum = *d_max;
-   //checkCudaErrors(cudaMemcpy(d_min, &min_logLum, sizeof(float), cudaMemcpyDeviceToHost));
-   //checkCudaErrors(cudaMemcpy(d_max, &max_logLum, sizeof(float), cudaMemcpyDeviceToHost));
+   checkCudaErrors(cudaMemcpy(&min_logLum, d_min, sizeof(float), cudaMemcpyDeviceToHost));
+   checkCudaErrors(cudaMemcpy(&max_logLum, d_max, sizeof(float), cudaMemcpyDeviceToHost));
+   
+   printf("%f\t%f\n", min_logLum, max_logLum);
    
    cudaFree(d_bins);
    cudaFree(d_min);
