b6f4ea0ee40477857ee9553d8d000d0692eef60e
funnymutant <fmutant@mutantNote.(none)>
2015-02-07 10:58:21 +0300

    2.18 ms

--- Problem Sets/Problem Set 5/student.cu
+++ Problem Sets/Problem Set 5/student.cu
@@ -1,30 +1,3 @@
-/* Udacity HW5
-   Histogramming for Speed
-
-   The goal of this assignment is compute a histogram
-   as fast as possible.  We have simplified the problem as much as
-   possible to allow you to focus solely on the histogramming algorithm.
-
-   The input values that you need to histogram are already the exact
-   bins that need to be updated.  This is unlike in HW3 where you needed
-   to compute the range of the data and then do:
-   bin = (val - valMin) / valRange to determine the bin.
-
-   Here the bin is just:
-   bin = val
-
-   so the serial histogram calculation looks like:
-   for (i = 0; i < numElems; ++i)
-     histo[val[i]]++;
-
-   That's it!  Your job is to make it run as fast as possible!
-
-   The values are normally distributed - you may take
-   advantage of this fact in your implementation.
-
-*/
-
-
 #include "utils.h"
 
 __global__
@@ -32,12 +5,21 @@ void yourHisto(const unsigned int* const vals, //INPUT
                unsigned int* const histo,      //OUPUT
                int numVals)
 {
-  //TODO fill in this kernel to calculate the histogram
-  //as quickly as possible
+   extern __shared__ unsigned int shmem[];
+   const size_t localId = threadIdx.x * numVals;
+   for(size_t i = 0; i < numVals; ++i)
+      shmem[localId + i] = 0;
 
-  //Although we provide only one kernel skeleton,
-  //feel free to use more if it will help you
-  //write faster code
+   __syncthreads();
+
+   size_t threadId = (threadIdx.x + blockDim.x * blockIdx.x) * numVals;
+   for (size_t i = 0; i < numVals; ++i)
+      atomicAdd(&shmem[vals[threadId + i]], 1);
+
+   __syncthreads();
+
+   for(size_t i = 0; i < numVals; ++i)
+      atomicAdd(&histo[localId + i], shmem[localId + i]);
 }
 
 void computeHistogram(const unsigned int* const d_vals, //INPUT
@@ -45,10 +27,11 @@ void computeHistogram(const unsigned int* const d_vals, //INPUT
                       const unsigned int numBins,
                       const unsigned int numElems)
 {
-  //TODO Launch the yourHisto kernel
+   const int threads = 256;
+   const int values = numBins / threads;
+   const int blocks = numElems / numBins;
+   yourHisto<<<blocks, threads, (numBins) * sizeof(unsigned int)>>>(d_vals, d_histo, values);
 
-  //if you want to use/launch more than one kernel,
-  //feel free
-
-  cudaDeviceSynchronize(); checkCudaErrors(cudaGetLastError());
+   cudaDeviceSynchronize();
+   checkCudaErrors(cudaGetLastError());
 }
